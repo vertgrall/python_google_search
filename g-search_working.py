f49b1a62da0fdcc1d4d7530769ec6086c3f40fd2
jon <vertgrall@gmail.com>
2018-09-18 18:57:11 -0700

    Add files via upload
    added user input for count

--- g-search_working.py
+++ g-search_working.py
@@ -7,8 +7,9 @@ import pickle
 import sys
 
 term = raw_input("Enter search subject: ")
+count = int(raw_input("enter how many results you want: "))
 lines = []
-for url in search(term, stop=20):
+for url in search(term, stop=count):
 #for url in search('"Seattle" Makers', stop=20):
     lines.append(url)
 outfile=open('./searchresults.txt', 'wb')
